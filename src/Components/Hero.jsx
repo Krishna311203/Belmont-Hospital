9c9861dbb8cbb844d386c4259f8127a04b081f41
Krishna311203 <krishnashrivastava84@gmail.com>
2023-08-12 16:12:38 +0530

    input added

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,36 +1,32 @@
 import React from "react";
 import "./Hero.css";
 import Cards from "./Cards";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 function Hero() {
   return (
     <>
-      
-       <div className="">
-          <div className=" left-side-gradient-div"> </div>
-          <div className=" right-side-gradient-div"> </div>
-       <div className="hero-div">
-        <h1 className="hero-title">
-          Your Mental <br></br> Health Matters
-        </h1>
-        <h3 className=" hero-text">Rediscover Happiness</h3>
-        <h2 className="hero-desc">
-          Ready to embrace a colorful life again? Embark on your healing journey
-          at<br /> Belmont Hospital.
-        </h2>
+      <div className="">
+        <div className=" left-side-gradient-div"> </div>
+        <div className=" right-side-gradient-div"> </div>
+        <div className="hero-div">
+          <h1 className="hero-title">
+            Your Mental <br></br> Health Matters
+          </h1>
+          <h3 className=" hero-text">Rediscover Happiness</h3>
+          <h2 className="hero-desc">
+            At Belmont Private Hospital, we proudly provide evidence-based
+            mental health treatment delivered by an experienced team of
+            healthcare professionals. <br/>We’re here to help with inpatient and day
+            therapy treatment options to help you or a loved one with their
+            mental health recovery.
+          </h2>
 
-        <Link to='/test' className="hero-button">
-          <p className="mr-2">Get Started</p>
-          <i className="fa-solid fa-arrow-right "></i>
-        </Link>
+          <Link to="/test" className="hero-button">
+            <p className="mr-2">Get Started</p>
+            <i className="fa-solid fa-arrow-right "></i>
+          </Link>
+        </div>
       </div>
-
-       </div>
-   
-       
-      
-
-      
     </>
   );
 }
--- src/Components/Test.jsx
+++ src/Components/Test.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import Cards from "./Cards";
+import "./Test.css";
 const Test = () => {
   return (
-    <div>
-      <div style={{ fontSize: 33, textAlign: "center", paddingTop: 200 }}>
-        test
+    <>
+      <div className="body">
+        <div className="card">
+          <h1 className="title">
+            Enter the following Details
+          </h1>
+          <div class="inputGroup">
+          <label for="Age">Age :</label>
+          <input type="email" placeholder="Enter Age in Years" id="email"/>
+        </div>
+        <div class="inputGroup">
+          <label for="password">RMT% :</label>
+          <input type="number" placeholder="Enter RMT%" id="password"/>
+        </div>
+        <button class="button-29"><span>Submit</span></button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
